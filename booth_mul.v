a2f0ab3a5a3cbcdf79c06fb2c650839b1d047996
Rukhsar Khan <53951120+laraib-786@users.noreply.github.com>
2019-10-06 20:50:15 +0530

    comment removed

--- booth_mul.v
+++ booth_mul.v
@@ -1,38 +1,3 @@
-/*module booth_mul(input signed [3:0] qr, br,
-output reg signed [7:0] ac);
-reg q0;
-reg [3:0] y;
-always@(qr, br)
-begin
-ac = 8'd0;
-q0 = 1'd0;
-if(qr[0]==0 && q0==1)
-begin
-ac[7:4]= ac[7:4] + br;
-q0 = qr[0];
-ac = ac[7:4] + qr;
-ac = ac >> 1;
-ac[7]=ac[6];
-end
-if(qr[0]==1 && q0==0)
-begin
-y = -br;
-ac[7:4] = ac[7:4] + y;
-q0 = qr[0];
-ac = ac[7:4] + qr;
-ac = ac >> 1;
-ac[7]= ac[6];
-end
-else
-begin
-q0 = qr[0];
-ac = ac[7:4]+qr;
-ac = ac>>1;
-ac[7]= ac[6];
-end
-end
-endmodule*/
-
 module booth_mul(input signed [3:0] qr, br,
 output reg signed [7:0] ac);
 reg [1:0] temp;
